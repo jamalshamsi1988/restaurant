7a02ec42cb445af455eee29df20e3f4e58f624e9
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-04-11 17:40:40 +0330

    Header is completed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { ApolloClient , InMemoryCache , ApolloProvider } from '@apollo/client';
 
 import App from './App';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
+const theme=createTheme({
+  palette :{
 
+    primary :{
+      main :"#000000"
+    },
+    secondary : {
+  main:"#ffff"
+    }
+  }
+})
 const client = new ApolloClient({
   uri:"https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clg7ys4tq1l7801uk6s4m349v/master" ,
   cache : new InMemoryCache()
@@ -13,7 +24,10 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
     <App />
+    </ThemeProvider>
   </ApolloProvider>
 );
 
