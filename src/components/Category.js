73df9d9903882b22b21f560f405876718ac246a1
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-04-14 17:35:02 +0330

    Category Cards import

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,51 +1,149 @@
-import React from 'react';
-import { Box, Stack ,Typography,styled} from '@mui/material';
+import React,{useState} from 'react';
+import { useQuery } from '@apollo/client';
 
-import Food from '../photos/Header (2).jpg';
+import { Typography,CardMedia, Card, CardHeader, Avatar, CardContent, CardActions,IconButton, Container} from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { red } from '@mui/material/colors';
 
 
-const StyledBox = styled(Box)({
-    height:300 ,
-    width:"100%" ,
-    cursor:"pointer",
-    backgroundRepeat:"no-repeat",
-    backgroundPosition:"center",
-    backgroundSize:"cover",
-});
+//Query
+import { GET_MENUS } from '../graphql/querys';
 
-const StyledTypography = styled(Typography)({
-    margin:"25% 50px 25% 50px",
-    background:"red",
-    opacity :"0.8",
-});
+import Food from "../photos/Header (1).jpg"
 
 const Category = () => {
-  return (
-    <Box>
-        <Stack direction={{xs:"column" , sm:"row" }} spacing={{xs:1,sm:2,md:4}} mt={5}>
-            <StyledBox sx={{backgroundImage:`url(${Food})`}} >
-                <StyledTypography align='center' variant='h3'>
-                        Menu
-                </StyledTypography>
+    const [expanded, setExpanded] = useState(false);
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+      };
+      const{loading , data , errors}=useQuery(GET_MENUS);
 
-            </StyledBox>
-
-            <StyledBox sx={{backgroundImage: `url(${Food})`}} >
-                <StyledTypography align='center' variant='h3'>
-                        Menu
-                </StyledTypography>
-
-            </StyledBox>
-
-            <StyledBox sx={{backgroundImage: `url(${Food})`}} >
-                <StyledTypography align='center' variant='h3'>
-                        Menu
-                </StyledTypography>
-
-            </StyledBox>
-        </Stack>
+      if(loading) return <h1>hi...</h1>
+      if(errors) return <h4>Error ...</h4>
       
-    </Box>
+    console.log({data});
+    
+  return (
+ <Container maxWidth="lg" spacing={3} padding={3} sx={{display:"flex", paddingTop:"55px"}} >
+
+    <Card sx={{ maxWidth: 345 ,marginLeft:5 , boxShadow:"rgba(0,0,0,0.1) 0 4px 12px" , borderRadius:4 }} xs={12} sm={6} md={4}  >
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={Food}
+        alt="Food"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+     
+      </Card>
+
+      <Card sx={{ maxWidth: 345 ,marginLeft:5 , boxShadow:"rgba(0,0,0,0.1) 0 4px 12px" , borderRadius:4 }}  >
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={Food}
+        alt="Food"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+     
+      </Card>
+      <Card sx={{ maxWidth: 345 ,marginLeft:5 , boxShadow:"rgba(0,0,0,0.1) 0 4px 12px" , borderRadius:4 }}  >
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={Food}
+        alt="Food"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+     
+      </Card>
+      </Container>
   )
 }
 
