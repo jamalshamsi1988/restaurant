724da994a82079f3ecf83daf55c095630eae7ea8
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-04-15 22:55:05 +0330

    Creat card component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/layout/Header';
 import CarouselHeader from './components/carousels/CarouselHeader';
 import Category from './components/Category';
 import MenuHeader from './components/MenuHeader';
+import Card from './components/cards/Card';
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
     <CarouselHeader />
     <Category/>
     <MenuHeader />
+    <Card />
     </>
   );
 }
--- src/components/MenuHeader.js
+++ src/components/MenuHeader.js
@@ -7,8 +7,8 @@ import Image from '../photos/01.jpg';
 const MenuHeader = () => {
   return (
     <div style={{position:"relative"}}>
-        <CardMedia component="img" image={Image}  />
-        <div style={{position:"absolute" , top:10 , left:"50%" , color:"white"}}>
+        <CardMedia component="img" image={Image} sx={{width:"100%" , height:"50vh"}} />
+        <div style={{position:"absolute" , top:90 , left:"45%" , color:"white" ,fontWeight:700, fontSize:"1.2rem"}}>
                 Restaurant Menu
         </div>
            
