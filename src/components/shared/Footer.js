0fd265fe96b5e98f0557fa3fae96d1accc826416
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-04-28 11:51:22 +0330

    footer component styled not completed

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,20 +1,39 @@
 import React from 'react'
-import {  FormControl, FormHelperText, Grid, Input, InputLabel, Typography } from '@mui/material'
+import { Button, FormControl, FormHelperText, FormLabel, Grid, Input, InputLabel, TextField, Typography } from '@mui/material'
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import PermPhoneMsgIcon from '@mui/icons-material/PermPhoneMsg';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import photo from '../../photos/Header (3).jpg'
-import CommentsBoxFooter from './CommentsBoxFooter'
+import AboutUs from './../layout/aboutUs/AboutUs';
+import { Link } from 'react-router-dom';
+
+
+
 
 const Footer = () => {
   return (
-    <Grid container style={{ position:"relative"}} mt={5}>
+    <Grid container spacing={{ xs: 2, md: 3 }}  mt={5} sx={{display:"flex" , flexWrap:"wrap",justifyContent:"space-between"}} >
    
-      <img  src={photo} alt='FooterPhoto' style={{height:"80vh" , width:"100%" , opacity:"0.7"}}/>
+      <img  src={photo} alt='FooterPhoto' style={{height:"85vh" , width:"100%" , opacity:"0.7"}}/>
+
+       <Grid item xs={2} sm={4} md={4}  sx={{position:"absolute" ,display:"flex",justifyContent:"space-between"}} >
+       <FmdGoodIcon />
+       <PermPhoneMsgIcon />
+       <AccessAlarmIcon/>
+       </Grid>
        
-        
- <FormControl sx={{display:"flex", flexDirection:"no-wrap",position:"absolute" , marginLeft:"10rem" ,border:"1px solid silver" , marginTop:5 , padding:5}} >
+<Grid xs={2} sm={4} md={4}  sx={{position:"absolute"}}><Link to="/AboutUs" style={{color:"#ffffff", textDecoration:"none"}}>About Us</Link></Grid>
+
+    <Grid item xs={2} sm={4} md={4}  mb={2} sx={{position:"absolute" ,
+ border:"1px solid silver" ,
+   marginTop:3,
+    }}  >  
+ <FormControl  >
        
    <Typography component="p" variant="h6"
-    style={{position:"absolute" , color:"#ffffff",alignItems:"center", textAlign:"center",backgroundColor:"#343A54" ,width:"100%" ,}}
-    
+    style={{position:"absolute" , color:"#ffffff",alignItems:"center", 
+    textAlign:"center",backgroundColor:"#343A54" ,width:"100%", gap: 2
+     }}
     fontWeight={700} >Contact Us</Typography>
  
   <FormControl sx={{marginTop:5}}>
@@ -38,8 +57,14 @@ const Footer = () => {
   <Input id="my-input" aria-describedby="my-helper-text" />
   <FormHelperText id="my-helper-text">Please Type Your Phon Number</FormHelperText>
   </FormControl>
-  <CommentsBoxFooter />
+  <FormControl>
+  <FormLabel sx={{color:"#ffffff"}}>Comment</FormLabel>
+  <TextField variant='outlined' minRows={3} /> 
+  <FormHelperText>Please Write Your Comment</FormHelperText>
 </FormControl>
+<Button size="sm" sx={{backgroundColor:"#343A54"}}>Send</Button>
+</FormControl>
+</Grid>
 
     </Grid>
   )
