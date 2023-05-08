6ab4a3b2d493cb94f346059611ccc337504d331e
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-05-08 17:59:29 +0330

    Create cartAction & cart Reducer

--- src/redux/cart/cartAction.js
+++ src/redux/cart/cartAction.js
@@ -4,16 +4,17 @@ const addItem=product=>{
         payload:product
     }
 }
+
 const removeItem=product=>{
     return{
-        type:"REMOVE_ITEM" ,
+        type :"REMOVE_ITEM" ,
         payload:product
     }
 }
 
 const increase=product=>{
     return{
-        type:"INCREASE" ,
+        type :"INCREASE" ,
         payload:product
     }
 }
@@ -24,16 +25,15 @@ const decrease=product=>{
         payload:product
     }
 }
+
 const checkout=()=>{
     return{
         type:"CHECKOUT"
-        
     }
 }
 const clear=()=>{
     return{
         type:"CLEAR"
-        
     }
 }
 
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,3 +1,6 @@
+import { AssignmentReturned } from "@mui/icons-material";
+
+
 const initialState={
     selectedItems:[],
     itemsCounter:0,
@@ -5,70 +8,69 @@ const initialState={
     checkOut:false
 }
 
-const sumItems= item =>{
-    const itemsCounter=item.reduce((total , product)=> total + product.quantity,0);
-    const total=item.reduce((total , product)=> total + product.price * product.quantity , 0).toFixed(2);
-    return ({itemsCounter , total});
+const sumItems=item =>{
+
+    const itemsCounter=initialState.selectedItems.reduce((total,product) => total + product.quantity,0);
+    const total=initialState.selectedItems.reduce((total,product) => total +  product.price  * product.quantity,0).toFixed(2);
+    return 
+    ({itemsCounter , total});
+
 }
 
-const cartReducer=(state=initialState , action)=>{
-
+const cartReducer = (state=initialState , action)=>{
     switch(action.type){
-        case "ADD_ITEM":
+        case "ADD_ITEM" : 
             if(!state.selectedItems.find(item => item.id === action.payload.id)){
                state.selectedItems.push({
                 ...action.payload ,
-                quantity:1
-            })
-            return {
+                quantity : 1
+               })
+               return{
                 ...state , selectedItems:[...state.selectedItems] ,
                 ...sumItems(state.selectedItems),
-                checkOut:false
+                checkOut : false
+               }
             }
-                
-            }
-            
-    case "REMOVE_ITEM":
-        const newSelectedItems=state.selectedItems.filter(item => item.id !==action.payload.id)
-        return {
-            ...state , selectedItems:[...newSelectedItems] ,
-            ...sumItems(newSelectedItems)
-        }
+            case "REMOVE_ITEM" :
 
-     case "INCREASE" :
-        const indexI=state.selectedItems.findIndex(item => item.id === action.payload.id);
-        state.selectedItems[indexI].quantity++;
-        return {
-            ...state ,
-            ...sumItems(state.selectedItems)
-        }
-
-        case "DECREASE" :
-            const indexD=state.selectedItems.findIndex(item => item.id === action.payload.id);
-            state.selectedItems[indexD].quantity--;
+            const newSelectedItems=state.selectedItems.filter(item => item.id !== action.payload.id);
             return{
-                ...state ,
-                ...sumItems(state.selectedItems)
-            }
-        
-        case "CHECKOUT":
-            return{
-                selectedItems:[],
-                itemsCounter:0,
-                total:0,
-                checkOut:true
+                ...state , selectedItems:[...newSelectedItems],
+                ...sumItems(newSelectedItems)
             }
 
-        case "CLEAR":
-            return{
-                selectedItems:[],
-                itemsCounter:0,
-                total:0,
-                checkOut:false
-            }
-        default :
-        return state;
+            case "INCREASE" :
+                const indexI= state.selectedItems.findIndex(item => item.id === action.payload.id);
+                state.selectedItems[indexI].quantity++;
+                return{
+                    ...state ,
+                    ...sumItems(state.selectedItems)
+                }
+
+             case "DECREADE" :
+                const indexD= state.selectedItems.findIndex(item => item.id === action.payload.id);
+                state.selectedItems[indexD].quantity--;
+                return{
+                    ...state ,
+                    ...sumItems(state.selectedItems)
+                }
+            case "CHECKOUT" :
+                return{
+                        selectedItems:[],
+                        itemsCounter:0,
+                        total:0,
+                        checkOut:true
+                }
+             case "CLEAR" :
+                return{
+                    selectedItems:[],
+                    itemsCounter:0,
+                    total:0,
+                    checkOut:false
+                }
+                default :
+                return state;
     }
-
 }
+
 export default cartReducer;
